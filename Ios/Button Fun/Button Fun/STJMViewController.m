f878e3f59aacfd9bad8633d41f80ceeaa9dbda14
Hu Zhe <jackhuzhe@163.com>
2014-03-25 13:38:09 +0800

    Initial Commit

--- Ios/Button Fun/Button Fun/STJMViewController.m
+++ Ios/Button Fun/Button Fun/STJMViewController.m
@@ -29,6 +29,16 @@
 - (IBAction)ButtonPressed:(UIButton *)sender {
     NSString *title = [sender titleForState:UIControlStateNormal];
     NSString *plainText = [NSString stringWithFormat:@"%@ button pressed.",title];
-    _statusLabel.text = plainText;
+    
+    NSMutableAttributedString *styledText = [[NSMutableAttributedString alloc] initWithString:plainText];
+    
+    NSDictionary *attributes =@{NSFontAttributeName:[UIFont boldSystemFontOfSize:_statusLabel.font.pointSize]};
+    
+    NSRange nameRange =[plainText rangeOfString:title];
+    
+    [styledText setAttributes:attributes range:nameRange];
+    
+//    _statusLabel.text = plainText;
+    _statusLabel.attributedText = styledText;
 }
 @end
---
+++ Ios/Control Fun/Control Fun/STJMViewController.m
@@ -0,0 +1,29 @@
+//
+//  STJMViewController.m
+//  Control Fun
+//
+//  Created by Hu Zhe on 14-3-25.
+//  Copyright (c) 2014年 Hu Zhe. All rights reserved.
+//
+
+#import "STJMViewController.h"
+
+@interface STJMViewController ()
+
+@end
+
+@implementation STJMViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
