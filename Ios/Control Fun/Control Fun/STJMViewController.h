9c5866f956dbf07657fbc3f274532d73b8221836
Hu Zhe <jackhuzhe@163.com>
2014-03-26 19:19:40 +0800

    finish example.

--- Ios/Control Fun/Control Fun/STJMViewController.h
+++ Ios/Control Fun/Control Fun/STJMViewController.h
@@ -8,11 +8,19 @@
 
 #import <UIKit/UIKit.h>
 
-@interface STJMViewController : UIViewController
+@interface STJMViewController : UIViewController<UIActionSheetDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *numberField;
 @property (weak, nonatomic) IBOutlet UITextField *nameField;
+@property (weak, nonatomic) IBOutlet UISwitch *leftSwitch;
+@property (weak, nonatomic) IBOutlet UISwitch *rightSwitch;
+@property (weak, nonatomic) IBOutlet UIButton *doSomethingButton;
 
 - (IBAction)textFieldDoneEditing:(id)sender;
 - (IBAction)backgroundTap:(id)sender;
+
+- (IBAction)switchChanged:(UISwitch *)sender;
+- (IBAction)toggleControls:(UISegmentedControl *)sender;
+- (IBAction)buttonPressed:(UIButton *)sender;
+
 
 @end
--- Ios/Control Fun/Control Fun/STJMViewController.m
+++ Ios/Control Fun/Control Fun/STJMViewController.m
@@ -35,4 +35,56 @@
     [self.numberField resignFirstResponder];
 }
 
+- (IBAction)switchChanged:(UISwitch *)sender {
+    BOOL setting = sender.isOn;
+    [self.leftSwitch setOn:setting animated:YES];
+    [self.rightSwitch setOn:setting animated:YES];
+}
+
+- (IBAction)toggleControls:(UISegmentedControl *)sender {
+    if (sender.selectedSegmentIndex==0) {
+        self.leftSwitch.hidden=NO;
+        self.rightSwitch.hidden=NO;
+        self.doSomethingButton.hidden=YES;
+    }
+    else{
+        self.leftSwitch.hidden=YES;
+        self.rightSwitch.hidden=YES;
+        self.doSomethingButton.hidden=NO;
+    }
+}
+
+- (IBAction)buttonPressed:(UIButton *)sender {
+    UIActionSheet *actionSheet = [[UIActionSheet alloc]
+                                  initWithTitle:@"Are you sure?"
+                                  delegate:self
+                                  cancelButtonTitle:@"No"
+                                  destructiveButtonTitle:@"Yes" otherButtonTitles:nil
+                                  ];
+    [actionSheet showInView:self.view];
+}
+
+-(void)actionSheet:(UIActionSheet *)actionSheet didDismissWithButtonIndex:(NSInteger)buttonIndex{
+    if (buttonIndex !=[actionSheet cancelButtonIndex]) {
+        NSString *msg = nil;
+        
+        if (self.nameField.text.length>0) {
+            msg = [NSString stringWithFormat:@"You can breathe easy, %@ ,everything went OK.",self.nameField.text];
+        } else {
+            msg = @"You can breathe easy, everything went OK.";        }
+            UIAlertView *alert = [[UIAlertView alloc]
+                                  initWithTitle:@"Something was done"
+                                  message:msg
+                                  delegate:self
+                                  cancelButtonTitle:@"Phew!"
+                                  otherButtonTitles:nil];
+            [alert show];
+
+        
+
+    }
+}
+
+
+
 @end
