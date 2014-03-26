a5067a9354b4221dd09664cb5e7daec88365f576
Hu Zhe <jackhuzhe@163.com>
2014-03-26 19:22:18 +0800

    Initial Commit

---
+++ Ios/View Switcher/View Switcher/STJMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  STJMAppDelegate.h
+//  View Switcher
+//
+//  Created by Hu Zhe on 14-3-26.
+//  Copyright (c) 2014年 Hu Zhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STJMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
