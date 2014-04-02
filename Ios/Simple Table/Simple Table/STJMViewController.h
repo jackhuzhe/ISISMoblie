3f0f5b52de8204451c9383ba0e0cac6a1f24314d
Hu Zhe <jackhuzhe@163.com>
2014-04-02 11:35:17 +0800

    Initial Commit

---
+++ Ios/Simple Table/Simple Table/STJMViewController.h
@@ -0,0 +1,13 @@
+//
+//  STJMViewController.h
+//  Simple Table
+//
+//  Created by Hu Zhe on 14-4-2.
+//  Copyright (c) 2014年 Hu Zhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STJMViewController : UIViewController
+
+@end
---
+++ Ios/Simple Table/Simple TableTests/Simple_TableTests.m
@@ -0,0 +1,34 @@
+//
+//  Simple_TableTests.m
+//  Simple TableTests
+//
+//  Created by Hu Zhe on 14-4-2.
+//  Copyright (c) 2014年 Hu Zhe. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Simple_TableTests : XCTestCase
+
+@end
+
+@implementation Simple_TableTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
