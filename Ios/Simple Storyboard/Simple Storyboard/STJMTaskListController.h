8126feb983b4d4b828cdd63766b887ae49603efb
Hu Zhe <jackhuzhe@163.com>
2014-04-09 20:39:55 +0800

    遮挡信号栏。

---
+++ Ios/Simple Storyboard/Simple Storyboard/STJMTaskListController.h
@@ -0,0 +1,15 @@
+//
+//  STJMTaskListController.h
+//  Simple Storyboard
+//
+//  Created by Hu Zhe on 14-4-9.
+//  Copyright (c) 2014年 Hu Zhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STJMTaskListController : UITableViewController
+
+@property (strong, nonatomic) NSArray *tasks;
+
+@end
