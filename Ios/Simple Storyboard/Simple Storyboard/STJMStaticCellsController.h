3ea105e1488d3b83cd9da90bb69a1428ae8560ae
Hu Zhe <jackhuzhe@163.com>
2014-04-10 17:27:04 +0800

    add static cell.

---
+++ Ios/Simple Storyboard/Simple Storyboard/STJMStaticCellsController.h
@@ -0,0 +1,15 @@
+//
+//  STJMStaticCellsController.h
+//  Simple Storyboard
+//
+//  Created by Hu Zhe on 14-4-10.
+//  Copyright (c) 2014年 Hu Zhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STJMStaticCellsController : UITableViewController
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+@property (weak, nonatomic) IBOutlet UILabel *timeLabel;
+
+@end
