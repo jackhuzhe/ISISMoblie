bba99b6d03d888c9e2fe44c1c7603e7fee4867ed
Hu Zhe <jackhuzhe@163.com>
2014-04-21 10:29:15 +0800

    Initial Commit

---
+++ Ios/Swipes/Swipes/STJMViewController.h
@@ -0,0 +1,13 @@
+//
+//  STJMViewController.h
+//  Swipes
+//
+//  Created by Hu Zhe on 14-4-21.
+//  Copyright (c) 2014年 Hu Zhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STJMViewController : UIViewController
+
+@end
---
+++ Ios/Swipes/SwipesTests/SwipesTests.m
@@ -0,0 +1,34 @@
+//
+//  SwipesTests.m
+//  SwipesTests
+//
+//  Created by Hu Zhe on 14-4-21.
+//  Copyright (c) 2014年 Hu Zhe. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SwipesTests : XCTestCase
+
+@end
+
+@implementation SwipesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
