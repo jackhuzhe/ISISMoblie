0ea7fe8f7070707991abed38d7ad129806b11ca9
Hu Zhe <jackhuzhe@163.com>
2014-05-13 10:40:18 +0800

    add job detail.

---
+++ Ios/ISIS2 Mobile/ISIS2 Mobile/STJMJobListController.h
@@ -0,0 +1,15 @@
+//
+//  STJMJobListController.h
+//  ISIS2 Mobile
+//
+//  Created by Hu Zhe on 14-5-9.
+//  Copyright (c) 2014年 Hu Zhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STJMJobListController : UITableViewController
+
+@property (strong, nonatomic) NSArray *jobs;
+
+@end
---
+++ Ios/ISIS2 Mobile/ISIS2 Mobile/STJMJobListController.m
@@ -0,0 +1,143 @@
+//
+//  STJMJobListController.m
+//  ISIS2 Mobile
+//
+//  Created by Hu Zhe on 14-5-9.
+//  Copyright (c) 2014年 Hu Zhe. All rights reserved.
+//
+
+#import "STJMJobListController.h"
+#import "STJMJob.h"
+
+@interface STJMJobListController ()
+
+@end
+
+@implementation STJMJobListController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    STJMJob *job1 = [STJMJob new];
+    job1.jobName = @"G_HOT";
+    job1.jobType = @"HOT";
+    
+    STJMJob *job2 = [STJMJob new];
+    job2.jobName = @"Net Fare Process";
+    job2.jobType = @"Net Fare Process";
+    
+    self.jobs = @[job1, job2];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+#warning Potentially incomplete method implementation.
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return [self.jobs count];
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    NSString *identifier  = @"completedItemCell";
+    STJMJob *job = [self.jobs objectAtIndex:indexPath.row];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    
+    // Configure the cell...
+    
+    NSMutableAttributedString *jobName = [[NSMutableAttributedString alloc] initWithString:job.jobName];
+    NSMutableAttributedString *jobType = [[NSMutableAttributedString alloc] initWithString:job.jobType];
+    
+    NSDictionary *textAttributes = @{NSFontAttributeName: [UIFont fontWithName:@"Courier" size:24], NSStrokeColorAttributeName:@3.0};
+
+    UILabel *cellJobNameLabel = (UILabel *)[cell viewWithTag:1];
+    cellJobNameLabel.attributedText = jobName;
+    
+    UILabel *cellJobTypeLabel = (UILabel *)[cell viewWithTag:2];
+    cellJobTypeLabel.attributedText = jobType;
+    
+    return cell;
+}
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
