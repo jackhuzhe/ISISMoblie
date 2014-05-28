950244a17bacb278cca59904ff5e9b41b182fe17
Hu Zhe <jackhuzhe@163.com>
2014-05-28 09:52:04 +0800

    add.

---
+++ Ios/ISIS2 Mobile/ISIS2 Mobile/STJMMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  STJMMainViewController.h
+//  ISIS2 Mobile
+//
+//  Created by Hu Zhe on 14-5-27.
+//  Copyright (c) 2014年 Hu Zhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STJMMainViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *sidebarButton;
+@end
